4393d160d072010cf8203e6f4854c89643f7b6e0
caroldbt <37454302+caroldbt@users.noreply.github.com>
2021-07-10 00:26:54 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,37 @@ namespace Eje171819
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             /************
+            * Ejercicio 17
+            * **********/
+
+            Console.WriteLine("Numeros del 1 al 100, bucle while");
+            int k = 1;
+            while (k <= 100)
+            {
+                Console.Write(k+" ");
+                k++;
+            }
+            /************
+            * Ejercicio 18
+            * **********/
+
+            Console.WriteLine("\nNumeros del 1 al 100, bucle for");
+            for (int g = 1; g <= 100; g++)
+            {
+                Console.Write(g+" ");
+            }
+
+            /************
+            * Ejercicio 19
+            * **********/
+
+            Console.WriteLine("\nNumeros del 1 al 100, divisibles entre 2 y 3");
+            for (int h = 1; h <= 100; h++)
+            {
+                if (h % 2 == 0 && h % 3 == 0)
+                    Console.Write(h+" ");
+            }
         }
     }
 }
